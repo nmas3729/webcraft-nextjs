534936b4f12d1e3332fd974fa5646f9e1b3094ca
NMAS INNOVATIONS <nmas3729@gmail.com>
2025-12-30 14:09:46 +0200

    Integrate Google Analytics in layout
    Added Google Analytics integration using Script component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Banner from './components/Banner';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import Script from 'next/script';  // ← This line is new (needed for Google Analytics)
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,21 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+        {/* Google Analytics - Start (everything below this line is new) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-P7B68F528Z"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-P7B68F528Z');
+          `}
+        </Script>
+        {/* Google Analytics - End */}
       </head>
       <body className={inter.className}>
         <Banner />
